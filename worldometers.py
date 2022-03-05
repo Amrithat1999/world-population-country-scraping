9dcd318ba44a886741704b94597190d09d6df7f5
Amritha T <93830629+Amrithat1999@users.noreply.github.com>
2022-03-05 09:47:16 +0530

    Add files via upload

---
+++ worldometers.py
@@ -0,0 +1,30 @@
+import scrapy
+
+class WorldometersSpider(scrapy.Spider):
+    name = 'worldometers'
+    allowed_domains = ['www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        rows=response.xpath('//tr')
+
+        for row in rows:
+            Countries=row.xpath('./td/a/text()').get()
+            Population=row.xpath('./td[3]/text()').get()
+            Yearly_change=row.xpath('./td[4]/text()').get()
+            Density = row.xpath('./td[6]/text()').get()
+            Land_area = row.xpath('./td[7]/text()').get()
+            Migrants = row.xpath('./td[8]/text()').get()
+            Med_age = row.xpath('./td[10]/text()').get()
+            Urban_pop = row.xpath('./td[11]/text()').get()
+
+            yield {
+            'Countries':Countries,
+            'Population':Population,
+            'Yearly_change':Yearly_change,
+            'Density':Density,
+            'Land_area':Land_area,
+            'Migrants':Migrants,
+            'Med_age':Med_age,
+            'Urban_pop':Urban_pop,
+            }
